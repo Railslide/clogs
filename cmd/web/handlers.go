2886736f239d77995632e48b95d7f6156e265b5f
Giulia Vergottini <hello@railslide.io>
2025-01-15 16:57:51 +0100

    Display list of ongoing routes

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -17,25 +17,26 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, sportRoute := range sportRoutes {
-		fmt.Fprintf(w, "%+v\n", sportRoute)
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/partials/nav.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
 	}
-	// files := []string{
-	// 	"./ui/html/base.tmpl.html",
-	// 	"./ui/html/partials/nav.tmpl.html",
-	// 	"./ui/html/pages/home.tmpl.html",
-	// }
-	// ts, err := template.ParseFiles(files...)
-	// if err != nil {
-	// 	app.serverError(w, r, err)
-	// 	return
-	// }
-	//
-	// err = ts.ExecuteTemplate(w, "base", nil)
-	// if err != nil {
-	// 	app.serverError(w, r, err)
-	// 	return
-	// }
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+        data := templateData{
+          SportRoutes: sportRoutes,
+        }
+
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
 }
 
 func (app *application) routeView(w http.ResponseWriter, r *http.Request) {
@@ -66,7 +67,11 @@ func (app *application) routeView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = ts.ExecuteTemplate(w, "base", sportRoute)
+        data := templateData{
+          SportRoute: sportRoute,
+        }
+
+	err = ts.ExecuteTemplate(w, "base", data)
 	if err != nil {
 		app.serverError(w, r, err)
 	}
---
+++ cmd/web/templates.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/railslide/clogs/internal/models"
+
+type templateData struct {
+	SportRoute  models.SportRoute
+	SportRoutes []models.SportRoute
+}
